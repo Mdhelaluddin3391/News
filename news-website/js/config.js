089ed00b07b65e76a8d0a1d4bbd8d8162307c85f
Mdhelaluddin3391 <Mdhelaluddin3391@gmail.com>
2026-03-22 23:01:54 +0530

    updates for docker

--- news-website/js/config.js
+++ news-website/js/config.js
@@ -1,7 +1,8 @@
 const CONFIG = {
-    // Agar production hai toh usi domain ka use karega, dev mein localhost
-    BACKEND_URL: window.location.origin, 
-    API_BASE_URL: `${window.location.origin}/api`, 
+    // Local development ke liye hum directly Django backend (port 8000) ko point karenge
+    BACKEND_URL: 'http://127.0.0.1:8000', 
+    API_BASE_URL: 'http://127.0.0.1:8000/api', 
+    
     GOOGLE_CLIENT_ID: '615098838513-hnphi7ekcv9nhjv94f0mfj0509nd63hu.apps.googleusercontent.com',
     VAPID_PUBLIC_KEY: 'BL_wQ4AU0MABrcB7uQc5dX7d725RZmGktXdlp9YD6m1MWopxpFcFMLjiBdF8pMjuAKOJmwX4a596wC0mj4HlMQ8'
 };

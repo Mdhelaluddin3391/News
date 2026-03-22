c5bd40b2db40f6f67fdc32904ce5f588be4eaeb4
Mdhelaluddin3391 <Mdhelaluddin3391@gmail.com>
2026-03-22 22:34:45 +0530

    updates for docker settings

--- news-website/js/config.js
+++ news-website/js/config.js
@@ -1,8 +1,7 @@
 const CONFIG = {
-    // Production me ise apne live backend domain se replace karein (e.g., 'https://api.dharmanagarlive.com')
-    BACKEND_URL: 'http://127.0.0.1:8000', 
-    API_BASE_URL: 'http://127.0.0.1:8000/api', 
+    // Agar production hai toh usi domain ka use karega, dev mein localhost
+    BACKEND_URL: window.location.origin, 
+    API_BASE_URL: `${window.location.origin}/api`, 
     GOOGLE_CLIENT_ID: '615098838513-hnphi7ekcv9nhjv94f0mfj0509nd63hu.apps.googleusercontent.com',
     VAPID_PUBLIC_KEY: 'BL_wQ4AU0MABrcB7uQc5dX7d725RZmGktXdlp9YD6m1MWopxpFcFMLjiBdF8pMjuAKOJmwX4a596wC0mj4HlMQ8'
 };
-

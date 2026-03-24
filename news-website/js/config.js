3cea24ab4ec378a27cadd8df56265223fbda103b
Mdhelaluddin3391 <Mdhelaluddin3391@gmail.com>
2026-03-24 23:44:12 +0530

    update for render

--- news-website/js/config.js
+++ news-website/js/config.js
@@ -1,9 +1,21 @@
-const CONFIG = {
-    // Production & Nginx reverse proxy ke liye empty aur relative path
-    BACKEND_URL: '', 
-    API_BASE_URL: '/api', 
+// const CONFIG = {
+//     // Production & Nginx reverse proxy ke liye empty aur relative path
+//     BACKEND_URL: '', 
+//     API_BASE_URL: '/api', 
     
-    // Aapki existing keys (Agar aapne live server ke liye nayi generate ki hain toh yahan replace kar lein)
+//     // Aapki existing keys (Agar aapne live server ke liye nayi generate ki hain toh yahan replace kar lein)
+//     GOOGLE_CLIENT_ID: '615098838513-hnphi7ekcv9nhjv94f0mfj0509nd63hu.apps.googleusercontent.com',
+//     VAPID_PUBLIC_KEY: 'BL_wQ4AU0MABrcB7uQc5dX7d725RZmGktXdlp9YD6m1MWopxpFcFMLjiBdF8pMjuAKOJmwX4a596wC0mj4HlMQ8'
+// };
+
+
+
+const CONFIG = {
+    // Backend ka naya live URL yahan set karein
+    BACKEND_URL: 'https://news-5p7p.onrender.com', 
+    API_BASE_URL: 'https://news-5p7p.onrender.com/api', 
+    
+    // Aapki existing keys
     GOOGLE_CLIENT_ID: '615098838513-hnphi7ekcv9nhjv94f0mfj0509nd63hu.apps.googleusercontent.com',
     VAPID_PUBLIC_KEY: 'BL_wQ4AU0MABrcB7uQc5dX7d725RZmGktXdlp9YD6m1MWopxpFcFMLjiBdF8pMjuAKOJmwX4a596wC0mj4HlMQ8'
 };

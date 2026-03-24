1910b92ec40d1763b8c70dab4550110a86e877ea
Mdhelaluddin3391 <Mdhelaluddin3391@gmail.com>
2026-03-24 14:23:29 +0530

    updates for production

--- news-website/js/config.js
+++ news-website/js/config.js
@@ -1,8 +1,9 @@
 const CONFIG = {
-    // Local development ke liye hum directly Django backend (port 8000) ko point karenge
-    BACKEND_URL: 'http://127.0.0.1:8000', 
-    API_BASE_URL: 'http://127.0.0.1:8000/api', 
+    // Production & Nginx reverse proxy ke liye empty aur relative path
+    BACKEND_URL: '', 
+    API_BASE_URL: '/api', 
     
+    // Aapki existing keys (Agar aapne live server ke liye nayi generate ki hain toh yahan replace kar lein)
     GOOGLE_CLIENT_ID: '615098838513-hnphi7ekcv9nhjv94f0mfj0509nd63hu.apps.googleusercontent.com',
     VAPID_PUBLIC_KEY: 'BL_wQ4AU0MABrcB7uQc5dX7d725RZmGktXdlp9YD6m1MWopxpFcFMLjiBdF8pMjuAKOJmwX4a596wC0mj4HlMQ8'
 };

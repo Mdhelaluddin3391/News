173a3a379c53fd7cb174536ca629b16a72108ed3
Mdhelaluddin3391 <Mdhelaluddin3391@gmail.com>
2026-04-05 13:00:46 +0530

    some udpates for prd

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -2,11 +2,11 @@
 domains=(feroxtimes.com www.feroxtimes.com)
 rsa_key_size=4096
 data_path="./certbot"
-email="feroxtimes@gmail.com" # <-- Fixed: Added the missing dot
+email="feroxtimes@gmail.com"
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  read -p "Existing data found for ${domains[*]}. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
